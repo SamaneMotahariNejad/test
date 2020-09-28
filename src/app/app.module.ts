6e7ebc0d6a3ddad8f12e3641edcd3b2562c53701
Samane Motaharinejad <s.motaharinejad@gmail.com>
2020-09-28 09:41:21 +0330

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TreeviewModule } from 'ngx-treeview';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TreeviewModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { TreeviewComponent, TreeviewConfig, TreeviewHelper, TreeviewItem } from 'ngx-treeview';
+import { remove } from 'lodash';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  items: TreeviewItem[];
+  Items;
+  selectedItem;
+  deleteEvent = false;
+  editChamp = false;
+
+  @Input() config: TreeviewConfig;
+  @ViewChild(TreeviewComponent, { static: false }) treeviewComponent: TreeviewComponent;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts(): TreeviewItem[] {
+    this.Items = new TreeviewItem({
+      text: 'Item One', value: 0, children: [
+        {
+          text: 'Item Two', value: 1
+        }
+      ]
+    });
+    this.selectedItem = this.Items.text;
+
+    return [this.Items];
+  }
+
+  removeItem(item: TreeviewItem): void {
+    for (const tmpItem of this.items) {
+      if (tmpItem === item) {
+        remove(this.items, item);
+      } else {
+        if (TreeviewHelper.removeItem(tmpItem, item)) {
+          break;
+        }
+      }
+    }
+
+    this.treeviewComponent.raiseSelectedChange();
+    this.deleteEvent = false;
+  }
+
+  onSelect(item) {
+    this.selectedItem = item.text;
+
+    if (item.value === 0) {
+      this.deleteEvent = false;
+      this.editChamp = true;
+    } else if (item.value === 1) {
+      this.deleteEvent = true;
+      this.editChamp = false;
+    }
+  }
+
+}
